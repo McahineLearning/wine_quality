0c199c5d81e86eb72ca843a3ba6bfd999b6fadf2
Ankan Datta <ankan.d@nabler.com>
2021-09-29 14:20:21 +0530

    stage 3 completed

--- src/load_data.py
+++ src/load_data.py
@@ -6,7 +6,16 @@ import argparse
 def load_and_save(config_path):
     config = read_params(config_path)
     df = get_data(config_path)
+    df['fixed acidity'].fillna(int(df['fixed acidity'].median()), inplace=True)
+    df['volatile acidity'].fillna(int(df['volatile acidity'].mean()), inplace=True)
+    df.dropna(inplace=True)
+    df.drop_duplicates(inplace=True)
     new_cols = [col.replace(" ", "_") for col in df.columns]
+    replace_map = {'type': {'white': 1, 'red': 2}}
+    types = df['type'].astype('category').cat.categories.tolist()
+    replace_map_comp = {'type': {k: v for k, v in zip(types, list(range(1, len(types) + 1)))}}
+    df.replace(replace_map_comp, inplace=True)
+    print(df.head())
     raw_data_path = config['load_data']['raw_dataset_csv']
 
     df.to_csv(raw_data_path, sep = ',', index = False, header = new_cols)
